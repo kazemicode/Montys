daafac639a28136fccc19b6cb1cff3f7a559733d
Nathan Warren-Acord <nacord26@gmail.com>
2019-08-03 22:41:06 -0700

    First take on design. Take two (different design) forthcoming.

--- app.js
+++ app.js
@@ -4,14 +4,26 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-const pool = require("./database");
+// const pool = require("./database");
 
 // Routes
 
 // Root route
-// app.get("/", async function(req, res) {
-    // Do stuff
-// });
+app.get("/", async function(req, res) {
+    res.render("index");
+});
+
+app.get("/products", async function(req, res) {
+    res.render("products");
+});
+
+app.get("/about", async function(req, res) {
+    res.render("about");
+});
+
+app.get("/faq", async function(req, res) {
+    res.render("faq");
+});
 
 // Server listener
 app.listen("8081", "127.0.0.1", function() {
