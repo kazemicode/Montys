65a1172c779eefea508a6fe03b1d96c9925a0059
rdorrity <37189242+rdorrity@users.noreply.github.com>
2019-08-13 08:54:01 -0700

    updating my branch from Dev

--- app.js
+++ app.js
@@ -155,10 +155,10 @@ function checkPassword(password, hashedValue) {
 }
 
 // Server listener
-// app.listen("8081", "127.0.0.1", function() {
-//     console.log("Express server is running...");
-// });
-
-app.listen(process.env.PORT, process.env.IP, function() {
-    console.log("Running Express Server...");
+app.listen("8081", "127.0.0.1", function() {
+    console.log("Express server is running...");
 });
+
+// app.listen(process.env.PORT, process.env.IP, function() {
+//     console.log("Running Express Server...");
+// });
